9e8aa09cae24bb3147205ab249885df56f8f6730
Budziaszek <budziaszek@gmail.com>
2020-11-22 15:32:41 +0100

    SummaryStatistics count, sum, custom_operation refactor

--- solution/summary_statistics.py
+++ solution/summary_statistics.py
@@ -48,25 +48,24 @@ class SummaryStatistics:
         if (key is None) is not (condition is None):
             raise ValueError(message)
 
-    def sum(self, key, distinct=False, filter_key=None, condition=None, group_by_key=None, reduce_function=None):
+    def _prepare_data(self, filter_key=None, condition=None, group_by_key=None, reduce_function=None):
         self._check(filter_key, condition, "Filtering data requires both filter_key and condition!")
         self._check(group_by_key, reduce_function, "Grouping data requires both group_by_key and reduce_function!")
-        data = self._group_by(group_by_key, reduce_function) if group_by_key else self.data
+        return self._group_by(group_by_key, reduce_function) if group_by_key else self.data
+
+    def sum(self, key, distinct=False, filter_key=None, condition=None, group_by_key=None, reduce_function=None):
+        data = self._prepare_data(filter_key, condition, group_by_key, reduce_function)
         if not distinct:
             return sum(self._get_by_key(key, filter_key, condition, data=data))
         return sum(set(self._get_by_key(key, filter_key, condition, data=data)))
 
     def count(self, key, distinct=False, filter_key=None, condition=None, group_by_key=None, reduce_function=None):
-        self._check(filter_key, condition, "Filtering data requires both filter_key and condition!")
-        self._check(group_by_key, reduce_function, "Grouping data requires both group_by_key and reduce_function!")
-        data = self._group_by(group_by_key, reduce_function) if group_by_key else self.data
+        data = self._prepare_data(filter_key, condition, group_by_key, reduce_function)
         res = self._get_by_key(key, filter_key, condition, data=data)
         return len(list(res) if not distinct else set(res))
 
     def custom_operation(self, fun, key, distinct=False, filter_key=None, condition=None,
                          group_by_key=None, reduce_function=None):
-        self._check(filter_key, condition, "Filtering data requires both filter_key and condition!")
-        self._check(group_by_key, reduce_function, "Grouping data requires both group_by_key and reduce_function!")
-        data = self._group_by(group_by_key, reduce_function) if group_by_key else self.data
+        data = self._prepare_data(filter_key, condition, group_by_key, reduce_function)
         res = self._get_by_key(key, filter_key, condition, data=data)
         return fun(list(res) if not distinct else set(res))
